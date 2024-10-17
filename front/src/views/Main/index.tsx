6ec94081b3d828d88de627700f0b4d6fbf6ab836
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2024-10-17 18:00:06 +0900

    플레이바 사운드바 기능 작성중

--- front/src/views/Main/index.tsx
+++ front/src/views/Main/index.tsx
@@ -3,9 +3,11 @@ import "./style.css";
 import ReactPlayer from "react-player";
 
 const Main = () => {
+  const [matchVideoUrl, setMatchVideoUrl] = useState<string>("");
   const [videoUrl, setVideoUrl] = useState<string>("");
   const [isPlaying, setIsPlaying] = useState<boolean>(false);
-  const [played, setPlayed] = useState<number>(0); // 진행률 (0 ~ 1)
+  // 진행률 (0 ~ 1)
+  const [played, setPlayed] = useState<number>(0);
   const playerRef = useRef<ReactPlayer | null>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -13,7 +15,13 @@ const Main = () => {
   };
 
   const handlePlayPause = () => {
-    setIsPlaying(!isPlaying);
+    // matchVideoUrl 값이 없을 경우(url을 입력하지 않았을 경우)
+    if (!matchVideoUrl) {
+      return;
+    } else {
+      // 있을 경우에는 play해줌
+      setIsPlaying(!isPlaying);
+    }
   };
 
   const [currentTime, setCurrentTime] = useState<number>(0); // 현재 재생 시간
@@ -43,7 +51,7 @@ const Main = () => {
   // 시간 계산
   function formatTime(time: number) {
     if (isNaN(time)) {
-      return `00:00`;
+      return;
     }
     const date = new Date(time * 1000);
     const hh = date.getUTCHours();
@@ -62,9 +70,16 @@ const Main = () => {
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
   ) => {
+    // 좌클릭(왼쪽 버튼)만 처리
+    if (event.button !== 0) return;
+
+    // 이벤트가 발생한 DOM 요소를 참조
     const progressBar = event.currentTarget;
+    // 진행 바의 크기와 위치 정보를 담고 있는 DOMRect 객체를 저장
     const rect = progressBar.getBoundingClientRect();
+    // 마우스 클릭 위치와 진행 바의 왼쪽 경계 간의 차이를 계산
     const offsetX = event.clientX - rect.left;
+    // 진행 바에서 클릭한 위치를 비율로 나타냄
     const newPlayed = offsetX / rect.width;
     handleSeek(newPlayed);
 
@@ -89,6 +104,72 @@ const Main = () => {
     }
   }, [played]);
 
+  const videoSearch = () => {
+    // "youtu"을 포함한 경우
+    if (videoUrl.includes("youtu")) {
+      if (videoUrl.includes("www.")) {
+        const urlMatch = videoUrl.match(/(?<=\?v=)[\w-]{11}/); // v= 다음의 값을 찾기
+        if (urlMatch) {
+          // 유튜브 영상 ID 추출 성공 시, 해당 ID로 matchVideoUrl 설정
+          setMatchVideoUrl(`youtu.be/${urlMatch[0]}`);
+          setIsPlaying(!isPlaying);
+        } else {
+          alert("v=이 없을 경우 실행. urlMatch 값 : " + urlMatch);
+          return;
+        }
+      } else {
+        setMatchVideoUrl(videoUrl);
+        setIsPlaying(!isPlaying);
+      }
+    } else {
+      // "youtu"를 포함하지 않은 경우
+      return;
+    }
+  };
+
+  //  볼륨 관련 =====================================
+  // 사운드바 드롭 상태
+  const [soundDropdownOpen, setSoundDropdownOpen] = useState<boolean>(true);
+
+  const onSoundDropDown = () => {
+    setSoundDropdownOpen(!soundDropdownOpen);
+  };
+
+  const [volume, setVolume] = useState<number>(1); // 초기 볼륨을 최대값으로 설정
+
+  const handleVolumeChange = (newVolume: number) => {
+    setVolume(newVolume);
+  };
+
+  const handleMouseDownVolume = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    if (event.button !== 0) return; // 좌클릭만 처리
+    // 이벤트가 발생한 DOM 요소를 참조
+    const volumeBar = event.currentTarget;
+    // 진행 바의 크기와 위치 정보를 담고 있는 DOMRect 객체를 저장
+    const rect = volumeBar.getBoundingClientRect();
+    // 마우스 클릭 위치와 진행 바의 왼쪽 경계 간의 차이를 계산
+    const offsetX = event.clientX - rect.left;
+    // 진행 바에서 클릭한 위치를 비율로 나타냄
+    const newVolume = Math.max(0, Math.min(1, offsetX / rect.width));
+    handleVolumeChange(newVolume); // 볼륨 업데이트
+
+    // const handleMouseMove = (moveEvent: MouseEvent) => {
+    //   const offsetX = moveEvent.clientX - rect.left;
+    //   const newVolume = Math.max(0, Math.min(1, offsetX / rect.height));
+    //   handleVolumeChange(newVolume); // 볼륨 업데이트
+    // };
+
+    // const handleMouseUp = () => {
+    //   window.removeEventListener("mousemove", handleMouseMove);
+    //   window.removeEventListener("mouseup", handleMouseUp);
+    // };
+
+    // window.addEventListener("mousemove", handleMouseMove);
+    // window.addEventListener("mouseup", handleMouseUp);
+  };
+
   return (
     <>
       <div className="main-wrap">
@@ -103,7 +184,7 @@ const Main = () => {
                 onChange={handleInputChange}
                 style={{ width: "400px", marginRight: "10px" }}
               />
-              <div className="main-search-btn" onClick={handlePlayPause}>
+              <div className="main-search-btn" onClick={videoSearch}>
                 {isPlaying ? ">" : ">>"}
               </div>
             </div>
@@ -173,31 +254,20 @@ const Main = () => {
                 <div
                   className="music-progress-bar-box"
                   onMouseDown={handleMouseDown}
-                  style={{
-                    width: "400px",
-                    height: "5px",
-                    background: "#3f3f3f",
-                    position: "relative",
-                    cursor: "pointer",
-                  }}
                 >
                   <div
                     className="music-progress-fill"
                     style={{
-                      background: "#cacaca",
-                      height: "100%",
                       width: `${played * 100}%`,
-                      position: "absolute",
-                      top: 0,
-                      left: 0,
                     }}
                   ></div>
                   <ReactPlayer
                     ref={playerRef}
-                    url={videoUrl}
+                    url={matchVideoUrl}
                     playing={isPlaying}
                     onProgress={handleProgress}
                     onDuration={handleDuration}
+                    volume={volume}
                     style={{ display: "none" }} // 완전히 숨김 처리
                   />
                 </div>
@@ -207,7 +277,25 @@ const Main = () => {
           </div>
 
           <div className="main-wrap-bottom-right">
-            <div className="music-sound-bar"></div>
+            {soundDropdownOpen ? (
+              <div className="music-sound-bar-container">
+                <div
+                  className="music-sound-bar"
+                  onMouseDown={handleMouseDownVolume}
+                >
+                  <div
+                    className="music-sound-bar-fill"
+                    style={{
+                      height: `${volume * 100}%`,
+                    }}
+                  ></div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+
+            <div className="music-sound-icon" onClick={onSoundDropDown}></div>
           </div>
         </div>
       </div>
