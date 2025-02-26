868ebb2d77adee3efa3da176e6a83821aad5b376
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2025-02-26 12:52:55 +0900

    기능에 사용할 사진 추가

--- https-music-player/src/main/java/com/hmplayer/https_music_player/domain/service/Musicservice.java
+++ https-music-player/src/main/java/com/hmplayer/https_music_player/domain/service/Musicservice.java
@@ -2,15 +2,10 @@ package com.hmplayer.https_music_player.domain.service;
 
 import com.hmplayer.https_music_player.domain.dto.request.AddPlayListToMusicRequest;
 import com.hmplayer.https_music_player.domain.dto.request.UpdatePlaylistOrderRequest;
-import com.hmplayer.https_music_player.domain.dto.response.music.CopyMusicResponse;
 import com.hmplayer.https_music_player.domain.dto.response.music.DeleteMusicResponse;
 import com.hmplayer.https_music_player.domain.dto.response.music.MusicResponse;
 import com.hmplayer.https_music_player.domain.dto.response.music.UpdateOrderValueResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 
 public interface Musicservice {
     ResponseEntity<? super MusicResponse> addPlayListToMusic(AddPlayListToMusicRequest request, String email);
