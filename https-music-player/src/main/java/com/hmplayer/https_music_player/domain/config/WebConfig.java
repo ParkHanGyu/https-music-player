d5e5e12040f9035344e29f3b346dd7bdecece621
DESKTOP-SFN0IMB\PHG <qkrgksrl0033@naver.com>
2025-05-14 17:01:52 +0900

    gitHub Pages CORS 변경

--- https-music-player/src/main/java/com/hmplayer/https_music_player/domain/config/WebConfig.java
+++ https-music-player/src/main/java/com/hmplayer/https_music_player/domain/config/WebConfig.java
@@ -16,7 +16,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3001") // 허용할 출처
+//                .allowedOrigins("http://localhost:3001")
+                .allowedOrigins("https://parkhangyu.github.io") // 허용할 출처
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메소드
                 .allowCredentials(true); // 쿠키 및 인증 정보를 포함하려면 true로 설정
     }
